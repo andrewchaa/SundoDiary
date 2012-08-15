9ba5c4505856d1aa892bd0685a4a76e3ca1c9c46
Andrew Chaa <andrew.chaa@huddle.net>
2012-08-15 17:29:55 +0100

    clean up pages

--- SundoDownloader.Domain/Repositories/DataPageParser.cs
+++ SundoDownloader.Domain/Repositories/DataPageParser.cs
@@ -5,14 +5,14 @@ namespace SundoDiary.Domain.Repositories
 {
     public class DataPageParser : IParse<DataPage>
     {
-        private static readonly Regex TitleRegex = new Regex("<title>(?<title>.*?)</title>", RegexOptions.IgnoreCase);
+        private static readonly Regex TitleRegex = new Regex("<title>(?<title>(.|\n)*?)</title>", RegexOptions.IgnoreCase);
         private static readonly Regex ContentRegex = new Regex("<content>(?<content>(.|\n)*?)</content>", RegexOptions.IgnoreCase);
 
         public DataPage Parse(string content)
         {
             return new DataPage
                        {
-                           Title = TitleRegex.Match(content).Groups["title"].Value, 
+                           Title = TitleRegex.Match(content).Groups["title"].Value.Replace(Environment.NewLine, "<br />"), 
                            Content = ContentRegex.Match(content).Groups["content"].Value.Replace(Environment.NewLine, "<br />")
                        };
         }
